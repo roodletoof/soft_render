2850189904090d6fca44c9d887d6e2b05c1f8e45
Ivar Fatland <ivar.fatland@lyse.net>
2024-07-27 17:11:00 +0200

    added masked drawing

--- sr.h
+++ sr.h
@@ -10,83 +10,27 @@ typedef struct sr_surface {
 
 void sr_pal(unsigned char from, unsigned char to);
 void sr_pal_reset();
+unsigned char sr_pal_transform(unsigned char from);
+unsigned char sr_get_pixel( sr_surface *self, int x, int y);
+void sr_pixel( sr_surface *self, unsigned char color, int x, int y);
+void sr_line( sr_surface *self, unsigned char color, int x0, int y0, int x1, int y1);
+void sr_rect_fill( sr_surface *self, unsigned char color, int x, int y, int width, int height);
+void sr_rect_outline( sr_surface *self, unsigned char color, int x, int y, int width, int height);
+void sr_circle_fill( sr_surface *self, unsigned char color, int x, int y, int radius);
+void sr_circle_outline( sr_surface *self, unsigned char color, int x, int y, int radius);
+void sr_blit( sr_surface *destination, int dest_x, int dest_y, sr_surface *source);
+void sr_blit_pro( sr_surface *destination, int dest_x, int dest_y, int dest_width, int dest_height, sr_surface *source, int source_x, int source_y, int source_width, int source_height);
+void sr_fill( sr_surface *self, unsigned char color);
 
-unsigned char sr_get_pixel(
-    sr_surface *self,
-    int x,
-    int y
-);
-
-void sr_pixel(
-    sr_surface *self,
-    unsigned char color,
-    int x,
-    int y
-);
-void sr_line(
-    sr_surface *self,
-    unsigned char color,
-    int x0,
-    int y0,
-    int x1,
-    int y1
-);
-
-void sr_rect_fill(
-    sr_surface *self,
-    unsigned char color,
-    int x,
-    int y,
-    int width,
-    int height
-);
-void sr_rect_outline(
-    sr_surface *self,
-    unsigned char color,
-    int x,
-    int y,
-    int width,
-    int height
-);
-
-void sr_circle_fill(
-    sr_surface *self,
-    unsigned char color,
-    int x,
-    int y,
-    int radius
-);
-void sr_circle_outline(
-    sr_surface *self,
-    unsigned char color,
-    int x,
-    int y,
-    int radius
-);
-
-void sr_blit(
-    sr_surface *destination,
-    int dest_x,
-    int dest_y,
-    sr_surface *source
-);
-void sr_blit_pro(
-    sr_surface *destination,
-    int dest_x,
-    int dest_y,
-    int dest_width,
-    int dest_height,
-    sr_surface *source,
-    int source_x,
-    int source_y,
-    int source_width,
-    int source_height
-);
-
-void sr_fill(
-    sr_surface *self,
-    unsigned char color
-);
+void sr_pixel_masked( sr_surface *self, unsigned char color, int x, int y);
+void sr_line_masked( sr_surface *self, unsigned char color, int x0, int y0, int x1, int y1);
+void sr_rect_fill_masked( sr_surface *self, unsigned char color, int x, int y, int width, int height);
+void sr_rect_outline_masked( sr_surface *self, unsigned char color, int x, int y, int width, int height);
+void sr_circle_fill_masked( sr_surface *self, unsigned char color, int x, int y, int radius);
+void sr_circle_outline_masked( sr_surface *self, unsigned char color, int x, int y, int radius);
+void sr_blit_masked( sr_surface *destination, int dest_x, int dest_y, sr_surface *source);
+void sr_blit_pro_masked( sr_surface *destination, int dest_x, int dest_y, int dest_width, int dest_height, sr_surface *source, int source_x, int source_y, int source_width, int source_height);
+void sr_fill_masked( sr_surface *self, unsigned char color);
 
 #define sr_surface_value(WIDTH, HEIGHT) \
     (sr_surface) { \
@@ -96,6 +40,8 @@ void sr_fill(
         .invisible = 0 \
     }
 
+// TODO remove this
+#define SR_IMPLEMENTATION
 #ifdef SR_IMPLEMENTATION
 
 static unsigned char color_map[256] = {
@@ -116,6 +62,7 @@ static unsigned char color_map[256] = {
     224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239,
     240, 241, 242, 243, 244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254, 255,
 };
+
 
 static inline void clamp_range(
         sr_surface *self,
@@ -150,6 +97,24 @@ static inline void clamp_range(
     }
 }
 
+static void circle_line(sr_surface *self, unsigned char color, int center_x, int center_y, int x, int y) {
+    sr_line(self, color, center_x+x, center_y+y, center_x+x, center_y-y);
+    sr_line(self, color, center_x-x, center_y+y, center_x-x, center_y-y);
+    sr_line(self, color, center_x+y, center_y+x, center_x+y, center_y-x);
+    sr_line(self, color, center_x-y, center_y+x, center_x-y, center_y-x);
+}
+
+static void circle_pixel(sr_surface *self, unsigned char color, int center_x, int center_y, int x, int y) {
+    sr_pixel(self, color, center_x+x, center_y+y);
+    sr_pixel(self, color, center_x-x, center_y+y);
+    sr_pixel(self, color, center_x+x, center_y-y);
+    sr_pixel(self, color, center_x-x, center_y-y);
+    sr_pixel(self, color, center_x+y, center_y+x);
+    sr_pixel(self, color, center_x-y, center_y+x);
+    sr_pixel(self, color, center_x+y, center_y-x);
+    sr_pixel(self, color, center_x-y, center_y-x);
+}
+
 unsigned char sr_get_pixel(
     sr_surface *self,
     int x,
@@ -164,6 +129,24 @@ unsigned char sr_get_pixel(
     return self->invisible;
 }
 
+static int abs(int i) {
+    return i < 0 ? -i : i;
+}
+
+void sr_pal(unsigned char from, unsigned char to) {
+    color_map[from] = to;
+}
+
+void sr_pal_reset() {
+    for (int i = 0; i < 256; ++i) {
+        color_map[i] = i;
+    }
+}
+
+unsigned char sr_pal_transform(unsigned char from) {
+    return color_map[from];
+}
+
 void sr_pixel(
     sr_surface *self,
     unsigned char color,
@@ -176,10 +159,6 @@ void sr_pixel(
     ) {
         self->pixels[y * self->width + x] = color_map[color];
     }
-}
-
-static int abs(int i) {
-    return i < 0 ? -i : i;
 }
 
 void sr_line(
@@ -255,13 +234,6 @@ void sr_rect_outline(
     }
 }
 
-static void circle_line(sr_surface *self, unsigned char color, int center_x, int center_y, int x, int y) {
-    sr_line(self, color, center_x+x, center_y+y, center_x+x, center_y-y);
-    sr_line(self, color, center_x-x, center_y+y, center_x-x, center_y-y);
-    sr_line(self, color, center_x+y, center_y+x, center_x+y, center_y-x);
-    sr_line(self, color, center_x-y, center_y+x, center_x-y, center_y-x);
-}
-
 void sr_circle_fill(
     sr_surface *self,
     unsigned char color,
@@ -284,17 +256,6 @@ void sr_circle_fill(
         }
         circle_line(self, color, x, y, outer_x, outer_y);
     }
-}
-
-static void circle_pixel(sr_surface *self, unsigned char color, int center_x, int center_y, int x, int y) {
-    sr_pixel(self, color, center_x+x, center_y+y);
-    sr_pixel(self, color, center_x-x, center_y+y);
-    sr_pixel(self, color, center_x+x, center_y-y);
-    sr_pixel(self, color, center_x-x, center_y-y);
-    sr_pixel(self, color, center_x+y, center_y+x);
-    sr_pixel(self, color, center_x-y, center_y+x);
-    sr_pixel(self, color, center_x+y, center_y-x);
-    sr_pixel(self, color, center_x-y, center_y-x);
 }
 
 void sr_circle_outline(
@@ -373,13 +334,211 @@ void sr_fill(
     }
 }
 
-void sr_pal(unsigned char from, unsigned char to) {
-    color_map[from] = to;
+static void circle_line_masked(sr_surface *self, unsigned char color, int center_x, int center_y, int x, int y) {
+    sr_line_masked(self, color, center_x+x, center_y+y, center_x+x, center_y-y);
+    sr_line_masked(self, color, center_x-x, center_y+y, center_x-x, center_y-y);
+    sr_line_masked(self, color, center_x+y, center_y+x, center_x+y, center_y-x);
+    sr_line_masked(self, color, center_x-y, center_y+x, center_x-y, center_y-x);
 }
 
-void sr_pal_reset() {
-    for (int i = 0; i < 256; ++i) {
-        color_map[i] = i;
+static void circle_pixel_masked(sr_surface *self, unsigned char color, int center_x, int center_y, int x, int y) {
+    sr_pixel_masked(self, color, center_x+x, center_y+y);
+    sr_pixel_masked(self, color, center_x-x, center_y+y);
+    sr_pixel_masked(self, color, center_x+x, center_y-y);
+    sr_pixel_masked(self, color, center_x-x, center_y-y);
+    sr_pixel_masked(self, color, center_x+y, center_y+x);
+    sr_pixel_masked(self, color, center_x-y, center_y+x);
+    sr_pixel_masked(self, color, center_x+y, center_y-x);
+    sr_pixel_masked(self, color, center_x-y, center_y-x);
+}
+
+
+void sr_pixel_masked(
+    sr_surface *self,
+    unsigned char color,
+    int x,
+    int y
+) {
+    if (
+        0 <= x && x < self->width &&
+        0 <= y && y < self->height
+    ) {
+        if (sr_get_pixel(self, x, y) == self->invisible) { return; }
+        self->pixels[y * self->width + x] = color_map[color];
+    }
+}
+
+void sr_line_masked(
+    sr_surface *self,
+    unsigned char color,
+    int x0,
+    int y0,
+    int x1,
+    int y1
+) {
+    // https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
+    int dx = abs(x1 - x0);
+    int sx = x0 < x1 ? 1 : -1;
+    int dy = -abs(y1 - y0);
+    int sy = y0 < y1 ? 1 : -1;
+    int error = dx + dy;
+    while (1) {
+        sr_pixel_masked( self, color, x0, y0 );
+        if ( x0 == x1 && y0 == y1 ) {
+            break;
+        }
+        int e2 = 2* error;
+        if ( e2 >= dy ) {
+            if ( x0 == x1 ) {
+                break;
+            }
+            error += dy;
+            x0 += sx;
+        }
+        if ( e2 <= dx ) {
+            if ( y0 == y1 ) {
+                break;
+            }
+            error += dx;
+            y0 += sy;
+        }
+    }
+}
+
+void sr_rect_fill_masked(
+    sr_surface *self,
+    unsigned char color,
+    int x,
+    int y,
+    int width,
+    int height
+) {
+    int start_x, start_y, stop_x, stop_y;
+    clamp_range(self, x, y, x+width, y+height, &start_x, &start_y, &stop_x, &stop_y);
+    for (int curr_y = start_y; curr_y < stop_y; curr_y++ ) {
+        for (int curr_x = start_x; curr_x < stop_x; curr_x++ ) {
+            if (self->pixels[curr_y * self->width + curr_x] == self->invisible) {continue;}
+            self->pixels[curr_y * self->width + curr_x] = color_map[color];
+        }
+    }
+}
+
+void sr_rect_outline_masked(
+    sr_surface *self,
+    unsigned char color,
+    int x,
+    int y,
+    int width,
+    int height
+) {
+    for ( int x2 = x; x2 < x + width; x2++ ) {
+        sr_pixel_masked( self, color, x2, y );
+        sr_pixel_masked( self, color, x2, y + height - 1 );
+    }
+    for ( int y2 = y+1; y2 < y + height - 1; y2++ ) {
+        sr_pixel_masked( self, color, x, y2 );
+        sr_pixel_masked( self, color, x + width - 1, y2 );
+    }
+}
+
+void sr_circle_fill_masked(
+    sr_surface *self,
+    unsigned char color,
+    int x,
+    int y,
+    int radius
+) {
+    // https://www.geeksforgeeks.org/bresenhams-circle-drawing-algorithm/
+    int outer_x = 0;
+    int outer_y = radius;
+    int d = 3 - 2 * radius;
+    circle_line_masked(self, color, x, y, outer_x, outer_y);
+    while ( outer_y >= outer_x ) {
+        outer_x++;
+        if ( d > 0 ) {
+            outer_y--;
+            d = d + 4 * (outer_x - outer_y) + 10;
+        } else {
+            d = d + 4 * outer_x + 6;
+        }
+        circle_line_masked(self, color, x, y, outer_x, outer_y);
+    }
+}
+
+void sr_circle_outline_masked(
+    sr_surface *self,
+    unsigned char color,
+    int x,
+    int y,
+    int radius
+) {
+    // https://www.geeksforgeeks.org/bresenhams-circle-drawing-algorithm/
+    int outer_x = 0;
+    int outer_y = radius;
+    int d = 3 - 2 * radius;
+    circle_pixel_masked(self, color, x, y, outer_x, outer_y);
+    while ( outer_y >= outer_x ) {
+        outer_x++;
+        if ( d > 0 ) {
+            outer_y--;
+            d = d + 4 * (outer_x - outer_y) + 10;
+        } else {
+            d = d + 4 * outer_x + 6;
+        }
+        circle_pixel_masked(self, color, x, y, outer_x, outer_y);
+    }
+}
+
+void sr_blit_masked(
+    sr_surface *destination,
+    int dest_x,
+    int dest_y,
+    sr_surface *source
+) {
+    int start_x, start_y, stop_x, stop_y;
+    clamp_range(destination, dest_x, dest_y, dest_x+source->width, dest_y+source->height, &start_x, &start_y, &stop_x, &stop_y);
+    for ( int y = start_y; y < stop_y; y++ ) {
+        for ( int x = start_x; x < stop_x; x++ ) {
+            unsigned char color = sr_get_pixel(source, x-start_x, y-start_y);
+            if (color == source->invisible) { continue; }
+            sr_pixel_masked(destination, color, x, y);
+        }
+    }
+}
+
+void sr_blit_pro_masked(
+    sr_surface *destination,
+    int dest_x,
+    int dest_y,
+    int dest_width,
+    int dest_height,
+    sr_surface *source,
+    int source_x,
+    int source_y,
+    int source_width,
+    int source_height
+) {
+    int start_x, start_y, stop_x, stop_y;
+    clamp_range(destination, dest_x, dest_y, dest_x+dest_width, dest_y+dest_height, &start_x, &start_y, &stop_x, &stop_y);
+    for (int y = start_y; y < stop_y; y++) {
+        for (int x = start_x; x < stop_x; x++) {
+            int s_x = source_x + (source_width) * (x-dest_x) / (dest_width);
+            int s_y = source_y + (source_height) * (y-dest_y) / (dest_height);
+            unsigned char color = sr_get_pixel(source, s_x, s_y);
+            if (color == source->invisible) { continue; }
+            sr_pixel_masked(destination, color, x, y);
+        }
+    }
+}
+
+void sr_fill_masked(
+    sr_surface *self,
+    unsigned char color
+) {
+    int buffer_size = self->width * self->height;
+    for (int i = 0; i < buffer_size; i++) {
+        if (self->pixels[i] == self->invisible) { continue; }
+        self->pixels[i] = color_map[color];
     }
 }
 
--- test.c
+++ test.c
@@ -12,23 +12,12 @@
 
 sr_surface *screen = &sr_surface_value(WIDTH, HEIGHT);
 
-// test definition
-#define RED "\033[31m"
-#define GREEN "\033[32m"
-#define DEFAULT "\033[39m"
-
-int tests_total = 0;
-int tests_passed = 0;
-typedef enum {result_success, result_failure} result_t;
-
 void ppm_save(const char* filename, sr_surface *surf) {
     FILE *f = fopen(filename, "wb");
     if (!f) {
         perror("file could not be opened");
     }
-
     fprintf(f, "P6\n%d\n%d\n255\n", surf->width, surf->height);
-
     for ( size_t y = 0; y < surf->height; y++ ) {
         for ( size_t x = 0; x < surf->width; x++ ) {
             unsigned char c = sr_get_pixel(surf, x, y);
@@ -39,12 +28,22 @@ void ppm_save(const char* filename, sr_surface *surf) {
     fclose(f);
 }
 
+
+// test definition
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define DEFAULT "\033[39m"
+
+int tests_total = 0;
+int tests_passed = 0;
+typedef enum {result_success, result_failure} result_t;
+
 result_t test(bool condition, char *condition_text, char *file, int line) {
 
     tests_total++;
     result_t result;
     if (condition) {
-        printf(GREEN " SUCCESS " DEFAULT "%s" "\n", condition_text);
+        // printf(GREEN " SUCCESS " DEFAULT "%s" "\n", condition_text);
         tests_passed++;
         result = result_success;
     } else {
@@ -65,15 +64,30 @@ result_t test(bool condition, char *condition_text, char *file, int line) {
 #define test(condition) test(condition, #condition, __FILE__, __LINE__)
 
 #define BLACK 0
+#define GRAY 127
 #define WHITE 255
 
 int main() {
 
+    screen->invisible = BLACK;
+
 
     { // TEST
-        sr_fill(screen, BLACK);
 
+        // circle
+        sr_fill(screen, BLACK);
         sr_circle_fill(screen, WHITE, 64, 64, 64);
+        test(sr_get_pixel(screen, 64, 64) == WHITE);
+        test(sr_get_pixel(screen, 64+63, 64) == WHITE);
+        test(sr_get_pixel(screen, 64, 64+63) == WHITE);
+        test(sr_get_pixel(screen, 64+65, 64) == BLACK);
+        test(sr_get_pixel(screen, 64, 64+65) == BLACK);
+        test(sr_get_pixel(screen, 128, 128) == BLACK);
+        sr_circle_fill_masked(screen, GRAY, 128, 64, 64);
+        test(sr_get_pixel(screen, 64+63, 64) == GRAY);
+        test(sr_get_pixel(screen, 64+65, 64) == BLACK);
+        test(sr_get_pixel(screen, 57, 64) == WHITE);
+        sr_fill_masked(screen, WHITE);
         test(sr_get_pixel(screen, 64, 64) == WHITE);
         test(sr_get_pixel(screen, 64+63, 64) == WHITE);
         test(sr_get_pixel(screen, 64, 64+63) == WHITE);
@@ -82,7 +96,6 @@ int main() {
         test(sr_get_pixel(screen, 128, 128) == BLACK);
 
         sr_fill(screen, BLACK);
-
         sr_circle_outline(screen, WHITE, 64, 64, 64);
         test(sr_get_pixel(screen, 64, 64) == BLACK);
         test(sr_get_pixel(screen, 64+64, 64) == WHITE);
@@ -92,19 +105,48 @@ int main() {
         test(sr_get_pixel(screen, 64+63, 64) == BLACK);
         test(sr_get_pixel(screen, 64, 64+63) == BLACK);
         test(sr_get_pixel(screen, 128, 128) == BLACK);
+        sr_fill(screen, BLACK);
+        sr_rect_fill(screen, GRAY, 0, 0, 70, 130);
+        sr_circle_outline_masked(screen, WHITE, 64, 64, 64);
+        test(sr_get_pixel(screen, 64, 64) == GRAY);
+        test(sr_get_pixel(screen, 64+64, 64) == BLACK);
+        test(sr_get_pixel(screen, 64, 64+64) == WHITE);
+        test(sr_get_pixel(screen, 64+65, 64) == BLACK);
+        test(sr_get_pixel(screen, 64, 64+65) == GRAY);
+        test(sr_get_pixel(screen, 64+63, 64) == BLACK);
+        test(sr_get_pixel(screen, 64, 64+63) == GRAY);
+        test(sr_get_pixel(screen, 128, 128) == BLACK);
 
+        // line
         sr_fill(screen, BLACK);
         sr_line(screen, WHITE, 0, 0, 200, 100);
         test(sr_get_pixel(screen, 0, 0) == WHITE);
         test(sr_get_pixel(screen, 200, 100) == WHITE);
         test(sr_get_pixel(screen, 200, 0) == BLACK);
         test(sr_get_pixel(screen, 0, 100) == BLACK);
+        sr_fill(screen, BLACK);
+        sr_rect_fill(screen, GRAY, 0, 0, 70, 130);
+        sr_line_masked(screen, WHITE, 0, 0, 200, 100);
+        test(sr_get_pixel(screen, 0, 0) == WHITE);
+        test(sr_get_pixel(screen, 200, 100) == BLACK);
+        test(sr_get_pixel(screen, 200, 0) == BLACK);
+        test(sr_get_pixel(screen, 0, 100) == GRAY);
 
+        // rect
         sr_fill(screen, BLACK);
         sr_rect_fill(screen, WHITE, 1, 1, 100, 100);
         test(sr_get_pixel(screen, 0, 0) == BLACK);
         test(sr_get_pixel(screen, 1, 1) == WHITE);
         test(sr_get_pixel(screen, 100, 100) == WHITE);
+        test(sr_get_pixel(screen, 101, 100) == BLACK);
+        test(sr_get_pixel(screen, 101, 101) == BLACK);
+        test(sr_get_pixel(screen, 50, 50) == WHITE);
+        sr_fill(screen, BLACK);
+        sr_rect_fill(screen, GRAY, 0, 0, 70, 130);
+        sr_rect_fill_masked(screen, WHITE, 1, 1, 100, 100);
+        test(sr_get_pixel(screen, 0, 0) == GRAY);
+        test(sr_get_pixel(screen, 1, 1) == WHITE);
+        test(sr_get_pixel(screen, 100, 100) == BLACK);
         test(sr_get_pixel(screen, 101, 100) == BLACK);
         test(sr_get_pixel(screen, 101, 101) == BLACK);
         test(sr_get_pixel(screen, 50, 50) == WHITE);
@@ -117,15 +159,134 @@ int main() {
         test(sr_get_pixel(screen, 101, 100) == BLACK);
         test(sr_get_pixel(screen, 101, 101) == BLACK);
         test(sr_get_pixel(screen, 50, 50) == BLACK);
+        sr_fill(screen, BLACK);
+        sr_rect_fill(screen, GRAY, 0, 0, 70, 130);
+        sr_rect_outline_masked(screen, WHITE, 1, 1, 100, 100);
+        test(sr_get_pixel(screen, 0, 0) == GRAY);
+        test(sr_get_pixel(screen, 1, 1) == WHITE);
+        test(sr_get_pixel(screen, 100, 100) == BLACK);
+        test(sr_get_pixel(screen, 101, 100) == BLACK);
+        test(sr_get_pixel(screen, 101, 101) == BLACK);
+        test(sr_get_pixel(screen, 50, 50) == GRAY);
+
+    }
+
+
+    { // TEST with other palette
+
+        sr_pal(BLACK, WHITE);
+        sr_pal(WHITE, GRAY);
+        sr_pal(GRAY, BLACK);
+
+        unsigned char map[256] = {0};
+        map[BLACK] = WHITE;
+        map[WHITE] = GRAY;
+        map[GRAY] = BLACK;
+        screen->invisible = WHITE;
+
+        // circle
+        sr_fill(screen, BLACK);
+        sr_circle_fill(screen, WHITE, 64, 64, 64);
+        test(sr_get_pixel(screen, 64, 64) == map[WHITE]);
+        test(sr_get_pixel(screen, 64+63, 64) == map[WHITE]);
+        test(sr_get_pixel(screen, 64, 64+63) == map[WHITE]);
+        test(sr_get_pixel(screen, 64+65, 64) == map[BLACK]);
+        test(sr_get_pixel(screen, 64, 64+65) == map[BLACK]);
+        test(sr_get_pixel(screen, 128, 128) == map[BLACK]);
+        sr_circle_fill_masked(screen, GRAY, 128, 64, 64);
+        test(sr_get_pixel(screen, 64+63, 64) == map[GRAY]);
+        test(sr_get_pixel(screen, 64+65, 64) == map[BLACK]);
+        test(sr_get_pixel(screen, 57, 64) == map[WHITE]);
+        sr_fill_masked(screen, WHITE);
+        test(sr_get_pixel(screen, 64, 64) == map[WHITE]);
+        test(sr_get_pixel(screen, 64+63, 64) == map[WHITE]);
+        test(sr_get_pixel(screen, 64, 64+63) == map[WHITE]);
+        test(sr_get_pixel(screen, 64+65, 64) == map[BLACK]);
+        test(sr_get_pixel(screen, 64, 64+65) == map[BLACK]);
+        test(sr_get_pixel(screen, 128, 128) == map[BLACK]);
+
+        sr_fill(screen, BLACK);
+        sr_circle_outline(screen, WHITE, 64, 64, 64);
+        test(sr_get_pixel(screen, 64, 64) == map[BLACK]);
+        test(sr_get_pixel(screen, 64+64, 64) == map[WHITE]);
+        test(sr_get_pixel(screen, 64, 64+64) == map[WHITE]);
+        test(sr_get_pixel(screen, 64+65, 64) == map[BLACK]);
+        test(sr_get_pixel(screen, 64, 64+65) == map[BLACK]);
+        test(sr_get_pixel(screen, 64+63, 64) == map[BLACK]);
+        test(sr_get_pixel(screen, 64, 64+63) == map[BLACK]);
+        test(sr_get_pixel(screen, 128, 128) == map[BLACK]);
+        sr_fill(screen, BLACK);
+        sr_rect_fill(screen, GRAY, 0, 0, 70, 130);
+        sr_circle_outline_masked(screen, WHITE, 64, 64, 64);
+        test(sr_get_pixel(screen, 64, 64) == map[GRAY]);
+        test(sr_get_pixel(screen, 64+64, 64) == map[BLACK]);
+        test(sr_get_pixel(screen, 64, 64+64) == map[WHITE]);
+        test(sr_get_pixel(screen, 64+65, 64) == map[BLACK]);
+        test(sr_get_pixel(screen, 64, 64+65) == map[GRAY]);
+        test(sr_get_pixel(screen, 64+63, 64) == map[BLACK]);
+        test(sr_get_pixel(screen, 64, 64+63) == map[GRAY]);
+        test(sr_get_pixel(screen, 128, 128) == map[BLACK]);
+
+        // line
+        sr_fill(screen, BLACK);
+        sr_line(screen, WHITE, 0, 0, 200, 100);
+        test(sr_get_pixel(screen, 0, 0) == map[WHITE]);
+        test(sr_get_pixel(screen, 200, 100) == map[WHITE]);
+        test(sr_get_pixel(screen, 200, 0) == map[BLACK]);
+        test(sr_get_pixel(screen, 0, 100) == map[BLACK]);
+        sr_fill(screen, BLACK);
+        sr_rect_fill(screen, GRAY, 0, 0, 70, 130);
+        sr_line_masked(screen, WHITE, 0, 0, 200, 100);
+        test(sr_get_pixel(screen, 0, 0) == map[WHITE]);
+        test(sr_get_pixel(screen, 200, 100) == map[BLACK]);
+        test(sr_get_pixel(screen, 200, 0) == map[BLACK]);
+        test(sr_get_pixel(screen, 0, 100) == map[GRAY]);
+
+        // rect
+        sr_fill(screen, BLACK);
+        sr_rect_fill(screen, WHITE, 1, 1, 100, 100);
+        test(sr_get_pixel(screen, 0, 0) == map[BLACK]);
+        test(sr_get_pixel(screen, 1, 1) == map[WHITE]);
+        test(sr_get_pixel(screen, 100, 100) == map[WHITE]);
+        test(sr_get_pixel(screen, 101, 100) == map[BLACK]);
+        test(sr_get_pixel(screen, 101, 101) == map[BLACK]);
+        test(sr_get_pixel(screen, 50, 50) == map[WHITE]);
+        sr_fill(screen, BLACK);
+        sr_rect_fill(screen, GRAY, 0, 0, 70, 130);
+        sr_rect_fill_masked(screen, WHITE, 1, 1, 100, 100);
+        test(sr_get_pixel(screen, 0, 0) == map[GRAY]);
+        test(sr_get_pixel(screen, 1, 1) == map[WHITE]);
+        test(sr_get_pixel(screen, 100, 100) == map[BLACK]);
+        test(sr_get_pixel(screen, 101, 100) == map[BLACK]);
+        test(sr_get_pixel(screen, 101, 101) == map[BLACK]);
+        test(sr_get_pixel(screen, 50, 50) == map[WHITE]);
+
+        sr_fill(screen, BLACK);
+        sr_rect_outline(screen, WHITE, 1, 1, 100, 100);
+        test(sr_get_pixel(screen, 0, 0) == map[BLACK]);
+        test(sr_get_pixel(screen, 1, 1) == map[WHITE]);
+        test(sr_get_pixel(screen, 100, 100) == map[WHITE]);
+        test(sr_get_pixel(screen, 101, 100) == map[BLACK]);
+        test(sr_get_pixel(screen, 101, 101) == map[BLACK]);
+        test(sr_get_pixel(screen, 50, 50) == map[BLACK]);
+        sr_fill(screen, BLACK);
+        sr_rect_fill(screen, GRAY, 0, 0, 70, 130);
+        sr_rect_outline_masked(screen, WHITE, 1, 1, 100, 100);
+        test(sr_get_pixel(screen, 0, 0) == map[GRAY]);
+        test(sr_get_pixel(screen, 1, 1) == map[WHITE]);
+        test(sr_get_pixel(screen, 100, 100) == map[BLACK]);
+        test(sr_get_pixel(screen, 101, 100) == map[BLACK]);
+        test(sr_get_pixel(screen, 101, 101) == map[BLACK]);
+        test(sr_get_pixel(screen, 50, 50) == map[GRAY]);
 
     }
 
     // RESULT MESSAGE
     if (tests_total == tests_passed) {
-        printf(GREEN "ALL TESTS PASSED" DEFAULT);
+        printf(GREEN "ALL %d TESTS PASSED" DEFAULT, tests_total);
         return 0;
     } else {
-        printf(RED "SOME TESTS FAILED" DEFAULT);
+        printf(RED "%d/%d TESTS FAILED" DEFAULT, tests_total - tests_passed, tests_total);
         return 1;
     }
 
